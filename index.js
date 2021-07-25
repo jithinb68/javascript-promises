792561832330bad194cf11b5ccfddcd877f085c3
jithin68 <jithin68@Jithin.local>
2021-07-25 11:53:28 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000  
+
+var courses = [
+    'javascript',
+    'php',
+    'python'
+]
+
+app.get('/', (request, response) => {
+    response.send('Hello World')
+})
+
+app.get('/api/courses', (request, response) => {
+    response.send(courses)
+})
+
+app.get('/api/courses/:id', (request, response) => {
+    id = request.params.id
+    response.send(courses[id]);
+})
+
+app.listen(3000, () => {
+    console.log(`listening on port ${port}...`);
+})
